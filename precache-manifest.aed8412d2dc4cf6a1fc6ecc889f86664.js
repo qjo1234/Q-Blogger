dee78f08042fa56ea8c70062b730fd64858dbec5
Qusai Obaid <58855181+qjo1234@users.noreply.github.com>
2020-08-19 21:24:13 +0300

    First Commit

---
+++ precache-manifest.aed8412d2dc4cf6a1fc6ecc889f86664.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca40a5043f41011af845787873a92090",
+    "url": "/Q-Blogger/index.html"
+  },
+  {
+    "revision": "7984d268f82749209c6a",
+    "url": "/Q-Blogger/static/css/main.ab3634e8.chunk.css"
+  },
+  {
+    "revision": "1748d9e492e788248a5b",
+    "url": "/Q-Blogger/static/js/2.a194547e.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/Q-Blogger/static/js/2.a194547e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7984d268f82749209c6a",
+    "url": "/Q-Blogger/static/js/main.8eebd48d.chunk.js"
+  },
+  {
+    "revision": "163431d091c52a435708f4c3bdf7aa48",
+    "url": "/Q-Blogger/static/js/main.8eebd48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce53a122d52b6b4320b",
+    "url": "/Q-Blogger/static/js/runtime-main.1b82682e.js"
+  },
+  {
+    "revision": "f58aa799509925974ae1fbbdeb0d85fb",
+    "url": "/Q-Blogger/static/media/logo-row.f58aa799.jpg"
+  },
+  {
+    "revision": "ed35934c8d23dc8ef060bda5debaa992",
+    "url": "/Q-Blogger/static/media/logo.ed35934c.png"
+  },
+  {
+    "revision": "df56f7c760a43e0cc70a85902ccd892e",
+    "url": "/Q-Blogger/static/media/not-found.df56f7c7.svg"
+  },
+  {
+    "revision": "7a3b6203b9320413ec360c8e5795e137",
+    "url": "/Q-Blogger/static/media/wallpaper.7a3b6203.svg"
+  },
+  {
+    "revision": "contact_v_1",
+    "url": "/Q-Blogger/pics/contact-us.png"
+  },
+  {
+    "revision": "find_v_1",
+    "url": "/Q-Blogger/pics/find.png"
+  },
+  {
+    "revision": "translation_v_1",
+    "url": "/Q-Blogger/pics/translation.png"
+  },
+  {
+    "revision": "translation_moblie_v_1",
+    "url": "/Q-Blogger/pics/translation-mobile.png"
+  },
+  {
+    "revision": "writer_v_1",
+    "url": "/Q-Blogger/pics/writer.png"
+  },
+  {
+    "revision": "favicon_v_1",
+    "url": "/Q-Blogger/pics/favicons/favicon.ico"
+  }
+]);
